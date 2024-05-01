4338bb8ea6f7033fac6731e8eeb7f7e7848d8634
thu4n <thuan.tongg@gmail.com>
2024-05-01 15:06:49 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# NT531 Project
+
+For all OpenFaaS related configurations and its functions, see the repo at: https://github.com/thu4n/NT533-AKS-OpenFaaS
--- qr-web/script.js
+++ qr-web/script.js
@@ -9,37 +9,33 @@ async function generateQR() {
   btn.setAttribute("disabled", true);
   btn.innerHTML = "Generating...";
 
-  const url = text.value.trim(); // Get the URL directly
+  const url = text.value.trim();
 
   try {
     const response = await fetch("https://faas.thu4n.dev/function/qrcode-go", {
       method: "POST",
       headers: {
-        "Content-Type": "text/plain", // Set appropriate content type for URL data
+        "Content-Type": "text/plain",
       },
-      body: url, // Send the URL directly as the body
+      body: url,
     });
 
     if (response.ok) {
-      // Success
       const blob = await response.blob();
       const url = window.URL.createObjectURL(blob); // Create temporary URL from blob
 
       img.src = url;
       img.classList.remove("hidden");
     } else {
-      // Error
-      console.error("Error:", response.statusText); // Assuming you just log the status text for debugging
-      // You can display a user-friendly error message here if desired
+      console.error("Error:", response.statusText);
     }
   } catch (error) {
     console.error("Error generating QR:", error);
   } finally {
     btn.removeAttribute("disabled");
     btn.innerHTML = "Generate";
-    window.URL.revokeObjectURL(url); // Revoke temporary URL when no longer needed (optional)
+    window.URL.revokeObjectURL(url); // Revoke temporary URL when no longer needed
   }
 }
 
-// Minor visual adjustment (assuming you have a hidden class for the button)
 btn.classList.remove("hidden");
